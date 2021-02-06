0fea40a68b6c871f9af2d0446272fedd05937c3f
etceterad <74021334+etceterad@users.noreply.github.com>
2021-02-06 14:46:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// let starter = document.querySelector('.starter'),
+// premium = document.querySelector('.premium'),
+// enterprise = document.querySelector('.enterprise'),
+// on = document.querySelector();
+
+// starter.onmouseover = function(event) {
+//     if
+//   };
+
+//   premium.onmouseover = function(event) {
+//     let target = event.target;
+//     target.classList.toogle(on);
+//   };
+
+//   enterprise.onmouseover = function(event) {
+//     let target = event.target;
+//     target.classList.toogle(on);
+//   };
+
+// price.addEventListener('mouseon', (e) => {
+// price.classList.toogle(on);
+// });
+
+// const price = document.querySelectorAll('.boxes'),
+// starter = price.querySelector('.starter'),
+// on = document.querySelector('.on');
+
+// price.addEventListener('mouseon', (e) =>{
+// e.target.classList.add('on');
+// });
+ 
+// price.forEach(e =>{
+// e.addEventListener('mouseover', (even) => {
+//     even.target.style.background = 'orange';
+// }); 
+// e.addEventListener('mouseout', (even) => {
+//     even.target.style.background = '';
+// }); 
+// });
